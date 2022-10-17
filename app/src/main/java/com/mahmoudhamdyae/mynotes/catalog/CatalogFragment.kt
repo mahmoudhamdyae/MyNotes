1101c93671ac40a9d69acb7d02946bb168869934
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-10-17 14:20:38 +0200

    Starting-Point

--- app/src/main/java/com/mahmoudhamdyae/mynotes/catalog/CatalogFragment.kt
+++ app/src/main/java/com/mahmoudhamdyae/mynotes/catalog/CatalogFragment.kt
@@ -1,6 +1,5 @@
 package com.mahmoudhamdyae.mynotes.catalog
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -48,11 +47,5 @@ class CatalogFragment: Fragment() {
 
     private fun editNote(note: Note) {
         findNavController().navigate(CatalogFragmentDirections.actionCatalogFragmentToEditorFragment(note))
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    override fun onResume() {
-        super.onResume()
-        binding.listNotes.adapter?.notifyDataSetChanged()
     }
 }
