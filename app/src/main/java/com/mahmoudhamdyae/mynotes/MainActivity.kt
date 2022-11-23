b912fb82a00a523cf265ad85ea58d65b4241bfb3
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-11-23 17:30:56 +0200

    Dagger-Hilt-Added

--- app/src/main/java/com/mahmoudhamdyae/mynotes/MainActivity.kt
+++ app/src/main/java/com/mahmoudhamdyae/mynotes/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mahmoudhamdyae.mynotes
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var toast : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/mahmoudhamdyae/mynotes/di/ApplicationModule.kt
@@ -0,0 +1,16 @@
+package com.mahmoudhamdyae.mynotes.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun getString() = "My Notes"
+}
